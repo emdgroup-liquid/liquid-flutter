65cbbbe8fabf7228be1e9ed5618a2e7f40db1cc3
Yannick Stolle <yannick.stolle@merckgroup.com>
2025-01-09 14:17:31 +0100

    chore add a threshold to golden tests to compensate platform differences

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,8 +1,27 @@
 import 'dart:async';
 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+
+import 'utils/compare_goldens.dart';
+
+const _kGoldenTestsThreshold = 0.5 / 100;
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   await loadAppFonts();
-  return testMain();
+  if (goldenFileComparator is LocalFileComparator) {
+    final testUrl = (goldenFileComparator as LocalFileComparator).basedir;
+
+    goldenFileComparator = LocalFileComparatorWithThreshold(
+      Uri.parse('$testUrl/test.dart'),
+      _kGoldenTestsThreshold,
+    );
+  } else {
+    throw Exception(
+      'Expected `goldenFileComparator` to be of type `LocalFileComparator`, '
+      'but it is of type `${goldenFileComparator.runtimeType}`',
+    );
+  }
+
+  await testMain();
 }
