666d9b96335581d9734ef4e17be01371a6855510
Yannick <yannick.stolle@gmail.com>
2025-02-10 13:00:32 +0100

    feat: add link processing support to LdText component
    - Introduce processLinks parameter to enable link detection
    - Add support for parsing URLs and Markdown-style links
    - Implement onLinkTap callback for link interaction
    - Create buildTextSpanWithLinks utility function for link rendering

--- lib/src/text.dart
+++ lib/src/text.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_flutter/liquid_flutter.dart';
 
@@ -86,9 +87,13 @@ class LdText extends StatelessWidget {
   final FontWeight? fontWeight;
   final Color? color;
 
+  final bool processLinks;
+
   final LdSize size;
 
   final LdTextType? type;
+
+  final void Function(String)? onLinkTap;
 
   const LdText(this.text,
       {Key? key,
@@ -98,8 +103,10 @@ class LdText extends StatelessWidget {
       this.decoration,
       this.size = LdSize.m,
       this.type = LdTextType.paragraph,
+      this.onLinkTap,
       this.fontWeight,
       this.lineHeight,
+      this.processLinks = false,
       this.color})
       : super(key: key);
 
@@ -109,21 +116,125 @@ class LdText extends StatelessWidget {
     final LdMute? ldMute = context.findAncestorWidgetOfExactType<LdMute>();
     final theme = LdTheme.of(context, listen: true);
 
+    final text =
+        type == LdTextType.caption ? this.text.toUpperCase() : this.text;
+
+    final style = ldBuildTextStyle(
+      LdTheme.of(context, listen: true),
+      type ?? LdTextType.paragraph,
+      size,
+      color: color ?? (ldMute != null ? theme.textMuted : null),
+      lineHeight: lineHeight,
+    );
+
+    if (processLinks) {
+      return RichText(
+        text: _buildTextSpanWithLinks(
+          text,
+          style: style,
+          theme: theme,
+          onTap: (p0) {
+            if (onLinkTap != null) {
+              onLinkTap!(p0);
+            }
+          },
+        ),
+        textAlign: textAlign ?? TextAlign.start,
+        maxLines: maxLines,
+        overflow: overflow ?? TextOverflow.visible,
+        textDirection: TextDirection.ltr,
+      );
+    }
+
     return Text(
-      type == LdTextType.caption ? text.toUpperCase() : text,
-      style: ldBuildTextStyle(
-        LdTheme.of(context, listen: true),
-        type ?? LdTextType.paragraph,
-        size,
-        color: color ?? (ldMute != null ? theme.textMuted : null),
-        lineHeight: lineHeight,
-      ),
+      text,
+      style: style,
       textAlign: textAlign,
       maxLines: maxLines,
       overflow: overflow,
       textDirection: TextDirection.ltr,
     );
   }
+}
+
+TextSpan _buildTextSpanWithLinks(
+  String text, {
+  TextStyle? style,
+  required LdTheme theme,
+  void Function(String)? onTap,
+}) {
+  final List<TextSpan> children = [];
+  final RegExp urlRegex = RegExp(
+    r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})',
+    caseSensitive: false,
+  );
+
+  final RegExp markdownLinkRegex = RegExp(r'\[(.*?)\]\((.*?)\)');
+
+  int lastMatchEnd = 0;
+  text.splitMapJoin(
+    RegExp('${urlRegex.pattern}|${markdownLinkRegex.pattern}'),
+    onMatch: (Match match) {
+      final matchedText = match.group(0)!;
+      final markdownMatch = markdownLinkRegex.firstMatch(matchedText);
+
+      if (markdownMatch != null) {
+        final linkText = markdownMatch.group(1)!;
+        final url = markdownMatch.group(2)!;
+        children.add(
+          TextSpan(
+            text: linkText,
+            style: style?.copyWith(
+              color: theme.primaryColor,
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                if (onTap != null) {
+                  onTap(url);
+                }
+              },
+          ),
+        );
+      } else {
+        children.add(
+          TextSpan(
+            text: matchedText,
+            style: style?.copyWith(
+              color: theme.primaryColor,
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                if (onTap != null) {
+                  onTap(matchedText);
+                }
+              },
+          ),
+        );
+      }
+      lastMatchEnd = match.end;
+      return '';
+    },
+    onNonMatch: (String text) {
+      if (text.isNotEmpty) {
+        children.add(TextSpan(
+          text: text,
+          style: style,
+        ));
+      }
+      return '';
+    },
+  );
+
+  if (lastMatchEnd < text.length) {
+    children.add(TextSpan(
+      text: text.substring(lastMatchEnd),
+      style: style,
+    ));
+  }
+
+  return TextSpan(children: children);
 }
 
 double ldLineHeight(LdTextType? type, {LdSize size = LdSize.m}) {
