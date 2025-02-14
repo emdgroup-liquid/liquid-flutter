6e7c1ac7cc13e707bb9e179fa7bc1a2c75f51bd8
Felix <mail@flx.es>
2025-02-14 11:30:41 +0100

    refactor: improve source code loading with a caching mechanism

--- example/lib/components/component_well.dart
+++ example/lib/components/component_well.dart
@@ -1,4 +1,3 @@
-import 'package:dart_style/dart_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
@@ -72,15 +71,21 @@ class _ComponentWellState extends State<ComponentWell> {
         ));
   }
 
+  static final sourceCodeCache = <String, String>{};
+
   /// Builds the child with a button to toggle between the demo and the source code.
   Widget _buildChildWithSourceCode(BuildContext context) {
     final _sourcePath = widget.showSourceCodeOptions?.path ??
         "lib${GoRouter.of(context).state?.pageKey.value}.dart";
     return FutureBuilder<String>(
-      future: rootBundle.loadString(_sourcePath),
+      future: sourceCodeCache.containsKey(_sourcePath)
+          ? Future.value(sourceCodeCache[_sourcePath])
+          : rootBundle.loadString(_sourcePath),
       builder: (context, snapshot) {
+        String? demoSource = "";
         if (snapshot.connectionState == ConnectionState.done) {
           if (snapshot.hasData) {
+            sourceCodeCache[_sourcePath] = snapshot.data!;
             RegExp exp = RegExp(
                 r'/\*\s*begin demo:\s*' +
                     widget.showSourceCodeOptions!.tag +
@@ -88,47 +93,37 @@ class _ComponentWellState extends State<ComponentWell> {
                     widget.showSourceCodeOptions!.tag +
                     r'\s*\*/',
                 dotAll: true);
-            var demoSource = exp
-                .firstMatch(snapshot.data!)
-                ?.group(1)
-                ?.trim()
-                // make it a valid dart statement by putting a semicolon at the end:
-                .replaceAll(RegExp(r',$'), ';');
-            if (demoSource != null) {
-              return Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: LdButtonGhost(
-                      leading: Icon(isShowingSourceCode
-                          ? Icons.grid_view_outlined
-                          : Icons.code),
-                      child: Text(isShowingSourceCode
-                          ? "Show Demo"
-                          : "Show Source Code"),
-                      onPressed: () => setState(
-                        () => isShowingSourceCode = !isShowingSourceCode,
-                      ),
-                    ),
-                  ),
-                  Visibility(
-                      child: widget.child, visible: !isShowingSourceCode),
-                  Visibility(
-                    child: CodeBlock(
-                      expanded: true,
-                      code: DartFormatter(
-                        languageVersion: DartFormatter.latestLanguageVersion,
-                        experimentFlags: [],
-                      ).formatStatement(demoSource),
-                    ),
-                    visible: isShowingSourceCode,
-                  ),
-                ],
-              );
-            }
+            demoSource = exp.firstMatch(snapshot.data!)?.group(1);
           }
         }
-        return widget.child;
+
+        return Column(
+          children: [
+            if (demoSource != null)
+              Align(
+                alignment: Alignment.centerRight,
+                child: LdButtonGhost(
+                  leading: Icon(isShowingSourceCode
+                      ? Icons.grid_view_outlined
+                      : Icons.code),
+                  child: Text(
+                      isShowingSourceCode ? "Show Demo" : "Show Source Code"),
+                  onPressed: () => setState(
+                    () => isShowingSourceCode = !isShowingSourceCode,
+                  ),
+                ),
+              ),
+            Visibility(child: widget.child, visible: !isShowingSourceCode),
+            if (demoSource?.isNotEmpty == true)
+              Visibility(
+                child: CodeBlock(
+                  code: demoSource!,
+                  expanded: true,
+                ),
+                visible: isShowingSourceCode,
+              ),
+          ],
+        );
       },
     );
   }
