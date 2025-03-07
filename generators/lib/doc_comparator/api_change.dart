b07362cdfa723169fc8a100bef1592930facb96b
Felix <mail@flx.es>
2025-03-07 12:54:33 +0100

    refactor: improve API change script output formatting

--- generators/lib/doc_comparator/api_change.dart
+++ generators/lib/doc_comparator/api_change.dart
@@ -1,39 +1,131 @@
+import 'package:liquid_generators/doc_items.dart';
+
 /// The type of change that was detected in the API.
 /// A [patch] change is a change that does not break the API.
 /// A [minor] change is a change that adds new features to the API or introduces
 /// backwards-compatible changes.
 /// A [major] change is a change that breaks the API or removes features.
-enum ApiChangeType {
+enum ApiChangeMagnitude {
   patch,
   minor,
   major;
 
-  ApiChangeType atLeast(ApiChangeType other) {
+  ApiChangeMagnitude atLeast(ApiChangeMagnitude other) {
     return index >= other.index ? this : other;
   }
 
-  ApiChangeType atMost(ApiChangeType other) {
+  ApiChangeMagnitude atMost(ApiChangeMagnitude other) {
     return index <= other.index ? this : other;
   }
+}
+
+enum ApiChangeOperation {
+  added,
+  removed,
+  typeChanged,
+  // Constructor Parameter changes:
+  becameOptional,
+  becameRequired,
+  becameNamed,
+  becamePositional,
+  becameNullUnsafe,
+  becameNullSafe,
+  becamePrivate,
+  becamePublic,
 }
 
 /// A change description in the API that belongs to a specific component.
 class ApiChange {
   final String component;
-  final String description;
-  final ApiChangeType type;
+  final ApiChangeOperation operation;
 
-  ApiChange({
+  ApiChange._({
     required this.component,
-    required this.description,
-    required this.type,
+    required this.operation,
   });
 
-  ApiChange atMost(ApiChangeType other) {
-    return ApiChange(
-      component: component,
-      description: description,
-      type: type.atMost(other),
-    );
+  ApiChangeMagnitude getMagnitude() {
+    if (component.startsWith('_')) {
+      // if the component is private, it's a patch change
+      return ApiChangeMagnitude.patch;
+    }
+    if (operation == ApiChangeOperation.added) {
+      // if a parameter, class or property was added, it's usually a minor
+      // change (unless it's private)
+      return ApiChangeMagnitude.minor;
+    }
+    // if we don't know the magnitude, we default to major (better to be safe)
+    return ApiChangeMagnitude.major;
+  }
+}
+
+class ComponentApiChange extends ApiChange {
+  ComponentApiChange({
+    required super.component,
+    required super.operation,
+  }) : super._();
+}
+
+class PropertyApiChange extends ApiChange {
+  final Property property;
+
+  PropertyApiChange({
+    required super.component,
+    required super.operation,
+    required this.property,
+  }) : super._();
+
+  @override
+  ApiChangeMagnitude getMagnitude() {
+    if (property.name.startsWith('_')) {
+      // if the property is private, it's a patch change
+      return ApiChangeMagnitude.patch;
+    }
+    return super.getMagnitude();
+  }
+}
+
+class ConstructorApiChange extends ApiChange {
+  final Constructor constructor;
+
+  ConstructorApiChange({
+    required super.component,
+    required super.operation,
+    required this.constructor,
+  }) : super._();
+
+  @override
+  ApiChangeMagnitude getMagnitude() {
+    if (constructor.name.startsWith('_')) {
+      // if the constructor is private, it's a patch change
+      return ApiChangeMagnitude.patch;
+    }
+    return super.getMagnitude();
+  }
+}
+
+class ConstructorParameterApiChange extends ApiChange {
+  final Constructor constructor;
+  final Parameter parameter;
+
+  ConstructorParameterApiChange({
+    required super.component,
+    required super.operation,
+    required this.constructor,
+    required this.parameter,
+  }) : super._();
+
+  @override
+  ApiChangeMagnitude getMagnitude() {
+    if (constructor.name.startsWith('_') || parameter.name.startsWith('_')) {
+      // if the constructor or parameter is private, it's a patch change
+      return ApiChangeMagnitude.patch;
+    }
+    if (operation == ApiChangeOperation.becameNullSafe ||
+        operation == ApiChangeOperation.becameOptional ||
+        (operation == ApiChangeOperation.removed && !parameter.required)) {
+      return ApiChangeMagnitude.minor;
+    }
+    return super.getMagnitude();
   }
 }
--- generators/lib/doc_comparator/doc_ext.dart
+++ generators/lib/doc_comparator/doc_ext.dart
@@ -2,25 +2,6 @@ import 'package:collection/collection.dart';
 import 'package:liquid_generators/doc_comparator/api_change.dart';
 import 'package:liquid_generators/doc_items.dart';
 
-extension ParameterStringExt on Parameter {
-  String get nameString => "${required ? "required " : "optional "}"
-      "${named ? "named " : ""}"
-      "${name.startsWith('_') ? "private " : ""}"
-      "parameter '$name'";
-  String get typeString => "type '$type'";
-}
-
-extension PropertyStringExt on Property {
-  String get nameString => "${name.startsWith('_') ? "private " : ""}"
-      "property '$name'";
-  String get typeString => "type '$type'";
-}
-
-extension ConstructorStringExt on Constructor {
-  String get nameString => "${name.startsWith("_") ? "private " : ""}"
-      "constructor '${name.isEmpty ? 'default' : name}'";
-}
-
 extension DocComponentListApiChangesExt on List<DocComponent> {
   List<ApiChange> compareTo(List<DocComponent> newComponents) {
     final changes = <ApiChange>[];
@@ -29,10 +10,9 @@ extension DocComponentListApiChangesExt on List<DocComponent> {
       final newComponent = newComponents
           .firstWhereOrNull((element) => element.name == this[i].name);
       if (newComponent == null) {
-        changes.add(ApiChange(
+        changes.add(ComponentApiChange(
           component: this[i].name,
-          description: "${this[i].name} was removed",
-          type: ApiChangeType.major,
+          operation: ApiChangeOperation.removed,
         ));
         continue;
       }
@@ -43,10 +23,9 @@ extension DocComponentListApiChangesExt on List<DocComponent> {
       final oldComponent =
           firstWhereOrNull((element) => element.name == newComponents[i].name);
       if (oldComponent == null) {
-        changes.add(ApiChange(
+        changes.add(ComponentApiChange(
           component: newComponents[i].name,
-          description: "${newComponents[i].name} was added",
-          type: ApiChangeType.minor,
+          operation: ApiChangeOperation.added,
         ));
       }
     }
@@ -59,18 +38,15 @@ extension DocComponentApiChangesExt on DocComponent {
   List<ApiChange> compareTo(DocComponent newComponent) {
     final changes = <ApiChange>[];
 
-    /// For components starting with an underscore, we consider changes to be at
-    /// most minor, as they are private to the library and should not be used
-    /// outside of it.
-    final atMostChangeType =
-        name.startsWith('_') ? ApiChangeType.patch : ApiChangeType.major;
-
-    if (isNullSafe && !newComponent.isNullSafe) {
-      changes.add(ApiChange(
-        component: name,
-        description: "Component became null-unsafe",
-        type: ApiChangeType.major.atMost(atMostChangeType),
-      ));
+    if (isNullSafe != newComponent.isNullSafe) {
+      changes.add(
+        ComponentApiChange(
+          component: name,
+          operation: newComponent.isNullSafe
+              ? ApiChangeOperation.becameNullSafe
+              : ApiChangeOperation.becameNullUnsafe,
+        ),
+      );
     }
 
     changes.addAll(
@@ -92,56 +68,41 @@ extension ConstructorApiChangesExt on Constructor {
     required String componentName,
   }) {
     final changes = <ApiChange>[];
-    final atMostChangeType = componentName.startsWith('_')
-        ? ApiChangeType.patch
-        : ApiChangeType.major;
+    _addChange(Parameter parameter, ApiChangeOperation operation) {
+      changes.add(ConstructorParameterApiChange(
+        component: componentName,
+        constructor: this,
+        operation: operation,
+        parameter: parameter,
+      ));
+    }
 
     for (var i = 0; i < signature.length; i++) {
       final oldParam = signature[i];
       final newParam = newConstructor.signature
           .firstWhereOrNull((element) => element.name == oldParam.name);
       if (newParam == null) {
-        changes.add(ApiChange(
-          component: componentName,
-          description:
-              "${oldParam.nameString} was removed in ${newConstructor.nameString}",
-          type: ApiChangeType.major.atMost(atMostChangeType).atMost(
-                oldParam.required ? ApiChangeType.major : ApiChangeType.minor,
-              ),
-        ));
+        _addChange(oldParam, ApiChangeOperation.removed);
         continue;
       }
-      if (!oldParam.required && newParam.required) {
-        changes.add(ApiChange(
-          component: componentName,
-          description:
-              "${oldParam.nameString} became required in ${newConstructor.nameString}",
-          type: ApiChangeType.major.atMost(atMostChangeType),
-        ));
+      if (oldParam.required != newParam.required) {
+        _addChange(
+          oldParam,
+          oldParam.required
+              ? ApiChangeOperation.becameOptional
+              : ApiChangeOperation.becameRequired,
+        );
       }
       if (oldParam.named != newParam.named) {
-        changes.add(ApiChange(
-          component: componentName,
-          description:
-              "${oldParam.nameString} named changed from ${oldParam.named} to ${newParam.named} in ${newConstructor.nameString}",
-          type: ApiChangeType.major.atMost(atMostChangeType),
-        ));
+        _addChange(
+          oldParam,
+          oldParam.named
+              ? ApiChangeOperation.becamePositional
+              : ApiChangeOperation.becameNamed,
+        );
       }
       if (oldParam.type != newParam.type) {
-        changes.add(ApiChange(
-          component: componentName,
-          description:
-              "${oldParam.nameString} type changed from ${oldParam.typeString} to ${newParam.typeString} in ${newConstructor.nameString}",
-          type: ApiChangeType.major.atMost(atMostChangeType),
-        ));
-      }
-      if (oldParam.required && !newParam.required) {
-        changes.add(ApiChange(
-          component: componentName,
-          description:
-              "${oldParam.nameString} became optional in ${newConstructor.nameString}",
-          type: ApiChangeType.minor,
-        ));
+        _addChange(oldParam, ApiChangeOperation.typeChanged);
       }
     }
 
@@ -150,12 +111,7 @@ extension ConstructorApiChangesExt on Constructor {
       final oldParam = signature
           .firstWhereOrNull((element) => element.name == newParam.name);
       if (oldParam == null) {
-        changes.add(ApiChange(
-          component: componentName,
-          description:
-              "${newParam.nameString} was added in ${newConstructor.nameString}",
-          type: newParam.required ? ApiChangeType.major : ApiChangeType.minor,
-        ));
+        _addChange(newParam, ApiChangeOperation.added);
       }
     }
 
@@ -171,18 +127,15 @@ extension ConstructorListApiChangesExt on List<Constructor> {
     required String componentName,
   }) {
     final changes = <ApiChange>[];
-    final atMostChangeType = componentName.startsWith('_')
-        ? ApiChangeType.patch // changes to private comps are considered patch
-        : ApiChangeType.major;
 
     for (var i = 0; i < length; i++) {
       final newConstructor = newConstructors
           .firstWhereOrNull((element) => element.name == this[i].name);
       if (newConstructor == null) {
-        changes.add(ApiChange(
+        changes.add(ConstructorApiChange(
           component: componentName,
-          description: "${this[i].nameString} was removed from $componentName",
-          type: ApiChangeType.major.atMost(atMostChangeType),
+          constructor: this[i],
+          operation: ApiChangeOperation.removed,
         ));
         continue;
       }
@@ -194,11 +147,10 @@ extension ConstructorListApiChangesExt on List<Constructor> {
       final oldConstructor = firstWhereOrNull(
           (element) => element.name == newConstructors[i].name);
       if (oldConstructor == null) {
-        changes.add(ApiChange(
+        changes.add(ConstructorApiChange(
           component: componentName,
-          description:
-              "${newConstructors[i].nameString} was added to $componentName",
-          type: ApiChangeType.minor,
+          constructor: this[i],
+          operation: ApiChangeOperation.added,
         ));
       }
     }
@@ -215,30 +167,23 @@ extension PropertyListApiChangesExt on List<Property> {
     required String componentName,
   }) {
     final changes = <ApiChange>[];
-    final componentAtMostChangeType = componentName.startsWith('_')
-        ? ApiChangeType.patch // changes to private comps are considered patch
-        : ApiChangeType.major;
 
     for (var i = 0; i < length; i++) {
       final newProperty = newProperties
           .firstWhereOrNull((element) => element.name == this[i].name);
-      final propertyAtMostChangeType = this[i].name.startsWith('_')
-          ? ApiChangeType.patch // changes to private props are considered patch
-          : componentAtMostChangeType;
       if (newProperty == null) {
-        changes.add(ApiChange(
+        changes.add(PropertyApiChange(
           component: componentName,
-          description: "${this[i].nameString} was removed",
-          type: ApiChangeType.major.atMost(propertyAtMostChangeType),
+          property: this[i],
+          operation: ApiChangeOperation.removed,
         ));
         continue;
       }
       if (this[i].type != newProperty.type) {
-        changes.add(ApiChange(
+        changes.add(PropertyApiChange(
           component: componentName,
-          description:
-              "${this[i].nameString} type changed from ${this[i].typeString} to ${newProperty.typeString}",
-          type: ApiChangeType.major.atMost(propertyAtMostChangeType),
+          property: this[i],
+          operation: ApiChangeOperation.typeChanged,
         ));
       }
     }
@@ -247,12 +192,10 @@ extension PropertyListApiChangesExt on List<Property> {
       final oldProperty =
           firstWhereOrNull((element) => element.name == newProperties[i].name);
       if (oldProperty == null) {
-        changes.add(ApiChange(
+        changes.add(PropertyApiChange(
           component: componentName,
-          description: "${newProperties[i].nameString} was added",
-          type: ApiChangeType.minor.atMost(newProperties[i].name.startsWith('_')
-              ? ApiChangeType.patch // private new props are considered patch
-              : componentAtMostChangeType),
+          property: newProperties[i],
+          operation: ApiChangeOperation.added,
         ));
       }
     }
