fb6c671a3136aec8a45bb3679f2712e76013bd25
Yannick <yannick.stolle@gmail.com>
2025-05-09 22:13:34 +0200

    refactor(badge): improve code formatting and readability in BadgeDemo component

--- example/lib/components/feedback/badge.dart
+++ example/lib/components/feedback/badge.dart
@@ -19,39 +19,43 @@ class BadgeDemo extends StatelessWidget {
           ComponentWell(
             child: LdAutoSpace(children: [
               ...LdSize.values.map(
-                (e) => Wrap(spacing: 8, runSpacing: 8, crossAxisAlignment: WrapCrossAlignment.center, children: [
-                  LdTextL(e.toString()),
-                  LdBadge(
-                    color: theme.primary,
-                    size: e,
-                    child: const Text("Hello"),
-                  ),
-                  LdBadge(
-                    color: theme.warning,
-                    size: e,
-                    child: const Text("Hello"),
-                  ),
-                  LdBadge(
-                    color: theme.success,
-                    size: e,
-                    child: const Text("Hello"),
-                  ),
-                  LdBadge(
-                    color: theme.secondary,
-                    size: e,
-                    child: const Text("Hello"),
-                  ),
-                  LdBadge(
-                    color: theme.error,
-                    size: e,
-                    child: const Text("Hello"),
-                  ),
-                ]),
+                (e) => Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      LdTextL(e.toString()),
+                      LdBadge(
+                        color: theme.primary,
+                        size: e,
+                        child: const Text("Hello"),
+                      ),
+                      LdBadge(
+                        color: theme.warning,
+                        size: e,
+                        child: const Text("Hello"),
+                      ),
+                      LdBadge(
+                        color: theme.success,
+                        size: e,
+                        child: const Text("Hello"),
+                      ),
+                      LdBadge(
+                        color: theme.secondary,
+                        size: e,
+                        child: const Text("Hello"),
+                      ),
+                      LdBadge(
+                        color: theme.error,
+                        size: e,
+                        child: const Text("Hello"),
+                      ),
+                    ]),
               ),
             ]),
           ),
           const LdTextP("symmetric = true, causes the badge to be a circle"),
-          const ComponentWell(
+          ComponentWell(
             child: Center(
               child: LdBadge(
                 symmetric: true,
