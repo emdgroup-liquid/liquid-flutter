8e18a299886e91a2c19a164852cdd5163964bf00
Yannick <yannick.stolle@gmail.com>
2025-05-09 22:14:54 +0200

    refactor(drawer): update DrawerDemo layout and enhance descriptions for better clarity and usability

--- example/lib/components/layout/drawer.dart
+++ example/lib/components/layout/drawer.dart
@@ -12,40 +12,61 @@ class DrawerDemo extends StatelessWidget {
     return ComponentPage(
         apiComponents: const ["LdDrawerItemSection", "LdSectionHeader"],
         title: "Drawer",
-        demo: ComponentWell(
-          child: Center(
-            child: Container(
-              width: 300,
-              padding: LdTheme.of(context).pad(size: LdSize.l),
-              decoration: BoxDecoration(
-                border: Border.all(width: 2, color: LdTheme.of(context).border),
+        demo: LdAutoSpace(
+          children: [
+            ComponentWell(
+              title: const LdTextHs("Standard drawer"),
+              description: const LdTextP(
+                "Liquid Flutter provides two components for making navigation drawers."
+                " The LdDrawerHeader and LdDrawerItemSection."
+                " They can ble placed in a Scaffolds drawer, or simply in a ListView for desktop use.",
               ),
-              child: const Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                LdDrawerHeader(title: Text("Header ")),
-                ldSpacerS,
-                LdSectionHeader("Section 1"),
-                ldSpacerS,
-                LdDrawerItemSection(active: true, leading: Icon(LucideIcons.circle), child: Text("Item 1")),
-                ldSpacerS,
-                LdDrawerItemSection(leading: Icon(LucideIcons.circle), child: Text("Item 2")),
-                ldSpacerS,
-                LdDrawerItemSection(
-                  leading: Icon(LucideIcons.circle),
-                  child: Text("Item 3"),
-                  children: [
-                    LdDrawerItemSection(child: Text("Item 3.1")),
-                    ldSpacerS,
-                    LdDrawerItemSection(child: Text("Item 3.2")),
-                    ldSpacerS,
-                    LdDrawerItemSection(
-                      trailing: Icon(LucideIcons.arrowRight),
-                      child: Text("Item 3.3"),
-                    )
-                  ],
-                )
-              ]),
+              child: Center(
+                child: Container(
+                  width: 300,
+                  padding: LdTheme.of(context).pad(size: LdSize.l),
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(width: 2, color: LdTheme.of(context).border),
+                  ),
+                  child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        LdDrawerHeader(title: Text("Header ")),
+                        ldSpacerS,
+                        LdSectionHeader("Section 1"),
+                        ldSpacerS,
+                        LdDrawerItemSection(
+                            active: true,
+                            leading: Icon(LucideIcons.circle),
+                            child: Text("Item 1")),
+                        ldSpacerS,
+                        LdDrawerItemSection(
+                            leading: Icon(LucideIcons.circle),
+                            child: Text("Item 2")),
+                        ldSpacerS,
+                        LdDrawerItemSection(
+                          leading: Icon(LucideIcons.circle),
+                          child: Text("Item 3"),
+                          children: [
+                            LdDrawerItemSection(child: Text("Item 3.1")),
+                            ldSpacerS,
+                            LdDrawerItemSection(child: Text("Item 3.2")),
+                            ldSpacerS,
+                            LdDrawerItemSection(
+                              trailing: Icon(LucideIcons.arrowRight),
+                              child: Text("Item 3.3"),
+                            )
+                          ],
+                        )
+                      ]),
+                ),
+              ),
             ),
-          ),
+            LdTextP(
+              "The LdDrawerItemSection component can be used to indicate a navigation item or a group of items. You can simply pass a list of LdDrawerItemSection to the children property to create a nested list.",
+            ),
+          ],
         ));
   }
 }
