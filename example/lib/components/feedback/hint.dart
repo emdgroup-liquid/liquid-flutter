02e3f0cc0d4433976cd3a2c4cfd527c074572d72
Yannick <yannick.stolle@gmail.com>
2025-05-09 22:13:48 +0200

    feat(hint): enhance HintDemo with additional hint types and improved description

--- example/lib/components/feedback/hint.dart
+++ example/lib/components/feedback/hint.dart
@@ -8,14 +8,24 @@ class HintDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ComponentPage(
+    return ComponentPage(
       title: "LdHint",
+      apiComponents: const ["LdHint", "LdHintType"],
       demo: ComponentWell(
+        onSurface: true,
+        description: const LdTextP(
+          "The LdHint component is a visual indicator paired with a text message. The status of the hint is an LdIndicator.",
+        ),
         child: LdAutoSpace(
           children: [
-            LdHint(type: LdHintType.info, child: Text("Hello world")),
-            LdHint(type: LdHintType.error, child: Text("Hello world")),
-            LdHint(type: LdHintType.warning, child: Text("Hello world"))
+            LdHint(type: LdHintType.info, child: Text("Info")),
+            LdHint(type: LdHintType.error, child: Text("Error")),
+            LdHint(type: LdHintType.warning, child: Text("Warning")),
+            LdHint(type: LdHintType.success, child: Text("Success")),
+            LdHint(type: LdHintType.canceled, child: Text("Canceled")),
+            LdHint(type: LdHintType.loading, child: Text("Loading")),
+            LdHint(type: LdHintType.pending, child: Text("Pending")),
+            LdHint(type: LdHintType.ongoing, child: Text("Ongoing")),
           ],
         ),
       ),
