33ca902e6f584a3f110369e350a3ad55edd76609
Yannick <yannick.stolle@gmail.com>
2025-05-09 22:18:24 +0200

    refactor(ld_selectable_list): streamline selection logic and enhance drag-and-drop functionality for improved user experience

--- lib/src/list/selectable_list.dart
+++ lib/src/list/selectable_list.dart
@@ -37,21 +37,26 @@ class LdSelectableList<T, GroupingCriterion> extends StatefulWidget {
   });
 
   @override
-  State<LdSelectableList<T, GroupingCriterion>> createState() => _LdSelectableListState<T, GroupingCriterion>();
+  State<LdSelectableList<T, GroupingCriterion>> createState() =>
+      _LdSelectableListState<T, GroupingCriterion>();
 }
 
-class _LdSelectableListState<T, GroupingCriterion> extends State<LdSelectableList<T, GroupingCriterion>>
-    with ChangeNotifier {
-  Set<T> selectedItems = {};
+class _LdSelectableListState<T, GroupingCriterion>
+    extends State<LdSelectableList<T, GroupingCriterion>> {
+  late final _selectedItems = _SetNotifier<T>(
+    {},
+    widget.multiSelect,
+  );
 
-  final FocusNode _focusNode = FocusNode();
+  late final _dragRectItems = _SetNotifier<T>(
+    {},
+    true,
+  );
 
+  final _changeNotifier = ChangeNotifier();
+
+  final _focusNode = FocusNode();
   late final ScrollController _scrollController;
-
-  final OverlayPortalController _overlayPortalController = OverlayPortalController();
-
-  Offset? _dragStartOffset = Offset.zero;
-  Offset? _dragEndOffset = Offset.zero;
 
   final Map<T, GlobalKey> _itemKeys = {};
 
@@ -64,156 +69,110 @@ class _LdSelectableListState<T, GroupingCriterion> extends State<LdSelectableLis
   void initState() {
     super.initState();
     _scrollController = ScrollController();
+    _selectedItems.addListener(() {
+      widget.onSelectionChange?.call(_selectedItems.value);
+
+      _changeNotifier.notifyListeners();
+    });
+    _dragRectItems.addListener(() {
+      _changeNotifier.notifyListeners();
+    });
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+    _changeNotifier.notifyListeners();
   }
 
   @override
   void dispose() {
     _focusNode.dispose();
-
+    _scrollController.dispose();
+    _selectedItems.dispose();
     super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(LdSelectableList<T, GroupingCriterion> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.multiSelect != widget.multiSelect) {
+      _selectedItems.allowMultiple = widget.multiSelect;
+    }
   }
 
   bool get isMultiSelect => widget.multiSelect;
 
   bool isSelected(T item) {
-    return selectedItems.contains(item) || _dragRectItems.contains(item);
-  }
-
-  void _addToSelection(T item) {
-    if (isMultiSelect) {
-      selectedItems.add(item);
-    } else {
-      selectedItems = {item};
+    if (_ctrlPressed || _shiftPressed) {
+      return _selectedItems.contains(item) || _dragRectItems.contains(item);
     }
-    notifyListeners();
-    widget.onSelectionChange?.call(selectedItems);
-  }
-
-  void _removeFromSelection(T item) {
-    selectedItems.remove(item);
-    notifyListeners();
-    widget.onSelectionChange?.call(selectedItems);
-  }
-
-  void _clearSelection() {
-    selectedItems.clear();
-    notifyListeners();
-    widget.onSelectionChange?.call(selectedItems);
-  }
-
-  void _addMultipleToSelection(Set<T> items) {
-    if (isMultiSelect) {
-      selectedItems.addAll(items);
-      notifyListeners();
-      widget.onSelectionChange?.call(selectedItems);
-    } else {
-      selectedItems = {items.first};
-      notifyListeners();
-      widget.onSelectionChange?.call(selectedItems);
+    if (_dragRectItems.value.isNotEmpty) {
+      return _dragRectItems.contains(item);
     }
+    return _selectedItems.contains(item);
   }
 
-  void _toggleItem(T item) {
-    if (selectedItems.contains(item)) {
-      _removeFromSelection(item);
-    } else {
-      _addToSelection(item);
-    }
-  }
-
+  // Called when the user taps an item
   void onTap(T item) {
     if (!isMultiSelect) {
-      _toggleItem(item);
+      _selectedItems.toggle(item);
       return;
     }
 
+    // We allow multi select and the user is holding shift
+    // We select all items between the last selected item and the current item
     if (_shiftPressed) {
-      final index = widget.paginator.items.indexOf(item);
-      final startIndex = widget.paginator.items.indexOf(selectedItems.last);
-
-      for (var i = min(startIndex, index); i <= max(startIndex, index); i++) {
-        final item = widget.paginator.items[i];
-        if (item != null) {
-          _addToSelection(item);
-        }
-      }
+      _selectRange(item);
     } else if (_ctrlPressed) {
-      _toggleItem(item);
+      // We allow multi select and the user is holding ctrl, therefore we toggle the item
+      // while keeping the other selected items
+      _selectedItems.toggle(item);
     } else {
-      _clearSelection();
-      _addToSelection(item);
+      // Only select the item the user tapped
+      _selectedItems.clear();
+      _selectedItems.add(item);
     }
   }
 
-  Rect? _dragRect() {
-    if (_dragStartOffset == null || _dragEndOffset == null) {
-      return null;
+  // Selects all items between the last selected item and the given item
+  void _selectRange(T end) {
+    final start = _selectedItems.value.last;
+    final startIndex = widget.paginator.items.indexOf(start);
+    final endIndex = widget.paginator.items.indexOf(end);
+
+    for (var i = min(startIndex, endIndex);
+        i <= max(startIndex, endIndex);
+        i++) {
+      final item = widget.paginator.items[i];
+      if (item != null) {
+        _selectedItems.add(item);
+      }
     }
-
-    final start = _dragStartOffset!;
-    final end = _dragEndOffset!;
-
-    final startX = min(start.dx, end.dx);
-    final startY = min(start.dy, end.dy);
-    final endX = max(start.dx, end.dx);
-    final endY = max(start.dy, end.dy);
-
-    return Rect.fromLTWH(
-      startX,
-      startY,
-      (endX - startX).abs(),
-      (endY - startY).abs(),
-    );
   }
 
-  bool _isWithinDragRect(Rect rect) {
-    final dragRect = _dragRect();
-
-    if (dragRect == null) {
-      return false;
-    }
-
-    return dragRect.overlaps(rect);
-  }
-
+  // Called when the user selects an item using the checkbox or radio
   void onSelectionChange(T item, bool selected) {
     if (isMultiSelect) {
       if (selected) {
         if (_shiftPressed) {
-          final index = widget.paginator.items.indexOf(item);
-          final startIndex = widget.paginator.items.indexOf(selectedItems.last);
-
-          for (var i = min(startIndex, index); i <= max(startIndex, index); i++) {
-            final item = widget.paginator.items[i];
-            if (item != null) {
-              selectedItems.add(item);
-            }
-          }
+          _selectRange(item);
         } else {
-          _addToSelection(item);
+          _selectedItems.add(item);
         }
       } else {
-        _removeFromSelection(item);
+        _selectedItems.remove(item);
       }
     } else {
       if (selected) {
-        _addToSelection(item);
+        _selectedItems.add(item);
       } else {
-        _clearSelection();
+        _selectedItems.clear();
       }
     }
-
-    notifyListeners();
   }
 
-  void clearSelection() {
-    selectedItems.clear();
-    notifyListeners();
-  }
-
-  final Set<T> _dragRectItems = {};
-
-  void _onUpdateDragRect() {
+  void _onUpdateDragRect(Rect dragRect) {
     for (final item in _itemKeys.entries) {
       final box = item.value.currentContext?.findRenderObject() as RenderBox?;
 
@@ -228,37 +187,191 @@ class _LdSelectableListState<T, GroupingCriterion> extends State<LdSelectableLis
         box.size.height,
       );
 
-      if (_isWithinDragRect(rect)) {
+      if (dragRect.overlaps(rect)) {
         _dragRectItems.add(item.key);
       } else {
         _dragRectItems.remove(item.key);
       }
     }
-
     setState(() {});
+  }
 
-    notifyListeners();
+  void _onEndDrag(Rect rect) {
+    if (_shiftPressed || _ctrlPressed) {
+      _selectedItems.addAll(_dragRectItems.value);
+    } else {
+      _selectedItems.clear();
+      _selectedItems.addAll(_dragRectItems.value);
+    }
+    _dragRectItems.clear();
+    setState(() {});
+  }
+
+  void _onCancel() {
+    _dragRectItems.clear();
+    setState(() {});
+  }
+
+  void _onKeyEvent(KeyEvent event) {
+    bool isShift = {
+      LogicalKeyboardKey.shiftLeft,
+      LogicalKeyboardKey.shiftRight,
+      LogicalKeyboardKey.shift,
+    }.contains(event.logicalKey);
+
+    bool isCtrl = {
+      LogicalKeyboardKey.controlLeft,
+      LogicalKeyboardKey.controlRight,
+      LogicalKeyboardKey.metaLeft,
+      LogicalKeyboardKey.metaRight,
+    }.contains(event.logicalKey);
+
+    if (isShift) {
+      _shiftPressed = event is KeyDownEvent;
+    }
+
+    if (isCtrl) {
+      _ctrlPressed = event is KeyDownEvent;
+    }
+
+    if (isShift || isCtrl) {
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _DragRect(
+        key: _rootKey,
+        onUpdateRect: _onUpdateDragRect,
+        onEndDrag: _onEndDrag,
+        onCancel: _onCancel,
+        child: KeyboardListener(
+            focusNode: _focusNode,
+            autofocus: true,
+            onKeyEvent: _onKeyEvent,
+            child: widget.listBuilder(context, _scrollController,
+                (context, item, index) {
+              if (!_itemKeys.containsKey(item)) {
+                _itemKeys[item] = GlobalKey();
+              }
+
+              return AnimatedBuilder(
+                  animation: _changeNotifier,
+                  key: _itemKeys[item],
+                  builder: (context, child) {
+                    return widget.itemBuilder(
+                      context: context,
+                      item: item,
+                      index: index,
+                      selected: isSelected(item),
+                      isMultiSelect: isMultiSelect,
+                      onSelectionChange: (selected) => onSelectionChange(
+                        item,
+                        selected,
+                      ),
+                      onTap: () => onTap(item),
+                    );
+                  });
+            })));
+  }
+}
+
+class _SetNotifier<T> extends ValueNotifier<Set<T>> {
+  _SetNotifier([Set<T>? value, this.allowMultiple = false])
+      : super(value ?? {});
+
+  bool allowMultiple;
+
+  void add(T item) {
+    if (allowMultiple) {
+      value = {...value, item};
+    } else {
+      value = {item};
+    }
+  }
+
+  void toggle(T item) {
+    if (contains(item)) {
+      remove(item);
+    } else {
+      add(item);
+    }
+  }
+
+  void remove(T item) {
+    value = {...value}..remove(item);
+  }
+
+  void clear() {
+    value = {};
+  }
+
+  void addAll(Set<T> items) {
+    if (allowMultiple) {
+      value = {...value, ...items};
+    } else {
+      value = {items.first};
+    }
+  }
+
+  bool contains(T item) => value.contains(item);
+}
+
+class _DragRect extends StatefulWidget {
+  final void Function(Rect rect) onUpdateRect;
+  final void Function(Rect rect) onEndDrag;
+  final void Function() onCancel;
+
+  final Widget child;
+
+  const _DragRect({
+    required this.onUpdateRect,
+    required this.onEndDrag,
+    required this.child,
+    required this.onCancel,
+    super.key,
+  });
+
+  @override
+  State<_DragRect> createState() => _DragRectState();
+}
+
+class _DragRectState extends State<_DragRect> {
+  Offset? _dragStartOffset;
+  Offset? _dragEndOffset;
+
+  final _overlayPortalController = OverlayPortalController();
+
+  Rect? get _dragRect {
+    if (_dragStartOffset == null || _dragEndOffset == null) {
+      return null;
+    }
+
+    final minX = min(_dragStartOffset!.dx, _dragEndOffset!.dx);
+    final minY = min(_dragStartOffset!.dy, _dragEndOffset!.dy);
+    final maxX = max(_dragStartOffset!.dx, _dragEndOffset!.dx);
+    final maxY = max(_dragStartOffset!.dy, _dragEndOffset!.dy);
+
+    return Rect.fromLTWH(minX, minY, (maxX - minX).abs(), (maxY - minY).abs());
   }
 
   @override
   Widget build(BuildContext context) {
     return OverlayPortal.targetsRootOverlay(
-      key: _rootKey,
       controller: _overlayPortalController,
       overlayChildBuilder: (context) {
-        if (_dragStartOffset == null || _dragEndOffset == null) {
+        final rect = _dragRect;
+        if (rect == null) {
           return const SizedBox.shrink();
         }
 
-        final sizeX = (_dragEndOffset!.dx - _dragStartOffset!.dx).abs();
-        final sizeY = (_dragEndOffset!.dy - _dragStartOffset!.dy).abs();
-
         return Positioned(
-          left: min(_dragStartOffset!.dx, _dragEndOffset!.dx),
-          top: min(_dragStartOffset!.dy, _dragEndOffset!.dy),
+          left: rect.left,
+          top: rect.top,
           child: Container(
-            width: sizeX,
-            height: sizeY,
+            width: rect.width,
+            height: rect.height,
             decoration: BoxDecoration(
               border: Border.all(
                 color: LdTheme.of(context).primaryColor,
@@ -269,126 +382,40 @@ class _LdSelectableListState<T, GroupingCriterion> extends State<LdSelectableLis
         );
       },
       child: GestureDetector(
-        onLongPressStart: (details) {
-          if (!isMultiSelect) {
-            return;
-          }
-
-          _dragRectItems.clear();
-          _dragStartOffset = details.globalPosition;
-          _overlayPortalController.show();
-          _dragEndOffset = null;
-          notifyListeners();
-        },
-        onLongPressEnd: (details) {
-          if (!isMultiSelect) {
-            return;
-          }
-          _dragEndOffset = null;
-          _overlayPortalController.hide();
-          _dragStartOffset = null;
-
-          if (_ctrlPressed) {
-            selectedItems.addAll(_dragRectItems);
-          } else {
-            selectedItems.clear();
-            selectedItems.addAll(_dragRectItems);
-          }
-          _dragRectItems.clear();
-          notifyListeners();
-        },
-        onLongPressMoveUpdate: (details) {
-          if (!isMultiSelect) {
-            return;
-          }
-          _dragEndOffset = details.globalPosition;
-          _onUpdateDragRect();
-          notifyListeners();
-        },
         onPanStart: (details) {
-          if (!isMultiSelect) {
-            return;
-          }
-
-          _dragRectItems.clear();
           _dragStartOffset = details.globalPosition;
           _overlayPortalController.show();
+
           _dragEndOffset = null;
-          notifyListeners();
         },
         onPanUpdate: (details) {
-          if (!isMultiSelect) {
+          _dragEndOffset = details.globalPosition;
+          final rect = _dragRect;
+          if (rect == null) {
             return;
           }
-
-          _dragEndOffset = details.globalPosition;
-          _onUpdateDragRect();
+          widget.onUpdateRect(rect);
         },
         onPanCancel: () {
           if (_dragStartOffset != null) {
             _dragEndOffset = null;
             _overlayPortalController.hide();
             _dragStartOffset = null;
-            _dragRectItems.clear();
-            notifyListeners();
+            widget.onCancel();
           }
         },
         onPanEnd: (details) {
-          if (!isMultiSelect) {
+          final rect = _dragRect;
+          if (rect == null) {
             return;
           }
 
           _dragEndOffset = null;
           _overlayPortalController.hide();
           _dragStartOffset = null;
-
-          if (_ctrlPressed) {
-            selectedItems.addAll(_dragRectItems);
-          } else {
-            selectedItems.clear();
-            selectedItems.addAll(_dragRectItems);
-          }
-          _dragRectItems.clear();
-          notifyListeners();
+          widget.onEndDrag(rect);
         },
-        child: KeyboardListener(
-          focusNode: _focusNode,
-          autofocus: true,
-          onKeyEvent: (event) {
-            if (event.logicalKey == LogicalKeyboardKey.shiftLeft || event.logicalKey == LogicalKeyboardKey.shiftRight) {
-              _shiftPressed = event is KeyDownEvent;
-            }
-            if (event.logicalKey == LogicalKeyboardKey.controlLeft ||
-                event.logicalKey == LogicalKeyboardKey.controlRight ||
-                event.logicalKey == LogicalKeyboardKey.metaLeft ||
-                event.logicalKey == LogicalKeyboardKey.metaRight) {
-              _ctrlPressed = event is KeyDownEvent;
-            }
-          },
-          child: widget.listBuilder(context, _scrollController, (context, item, index) {
-            if (!_itemKeys.containsKey(item)) {
-              _itemKeys[item] = GlobalKey();
-            }
-
-            return AnimatedBuilder(
-                animation: this,
-                key: _itemKeys[item],
-                builder: (context, child) {
-                  return widget.itemBuilder(
-                    context: context,
-                    item: item,
-                    index: index,
-                    selected: isSelected(item),
-                    isMultiSelect: isMultiSelect,
-                    onSelectionChange: (selected) => onSelectionChange(
-                      item,
-                      selected,
-                    ),
-                    onTap: () => onTap(item),
-                  );
-                });
-          }),
-        ),
+        child: widget.child,
       ),
     );
   }
