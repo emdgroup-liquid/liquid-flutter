cccdc4e414b35491f2b3c336e30c2a2b4e3a3c76
Yannick <yannick.stolle@gmail.com>
2025-06-14 11:40:02 +0200

    chore: improve select demo

--- example/lib/components/form_elements/select.dart
+++ example/lib/components/form_elements/select.dart
@@ -21,61 +21,63 @@ class _SelectDemoState extends State<SelectDemo> {
     return ComponentPage(
       path: "lib/components/form_elements/select.dart",
       title: "LdSelect",
-      demo: ComponentWell(
-        color: _onSurface ? theme.surface : theme.background,
-        child: Column(
-          children: [
-            LdSelect<String>(
-              label: "This is a label",
-              onSurface: _onSurface,
-              value: _value,
-              disabled: _disabled,
-              onChange: (p0) {
-                setState(() {
-                  _value = p0;
-                });
-              },
-              items: List.generate(
-                  32,
-                  (index) => LdSelectItem(
-                      value: index.toString(), child: Text("Choice $index"))),
-            ),
-            ldSpacerL,
-            LdSelect<String>(
-              onSurface: _onSurface,
-              value: _value,
-              disabled: _disabled,
-              onChange: (p0) {
-                setState(() {
-                  _value = p0;
-                });
-              },
-              items: const [
-                LdSelectItem(child: Text("Hello world"), value: "test"),
-                LdSelectItem(child: Text("Hello world 2"), value: "test2")
+      demo: LdAutoSpace(
+        children: [
+          ComponentWell(
+            color: _onSurface ? theme.surface : theme.background,
+            child: LdAutoSpace(
+              children: [
+                LdSelect<String>(
+                  label: "This is a label",
+                  onSurface: _onSurface,
+                  value: _value,
+                  disabled: _disabled,
+                  onChange: (p0) {
+                    setState(() {
+                      _value = p0;
+                    });
+                  },
+                  items: List.generate(
+                      32,
+                      (index) => LdSelectItem(
+                          value: index.toString(),
+                          child: Text("Choice $index"))),
+                ),
+                LdSelect<String>(
+                  onSurface: _onSurface,
+                  value: _value,
+                  disabled: _disabled,
+                  onChange: (p0) {
+                    setState(() {
+                      _value = p0;
+                    });
+                  },
+                  items: const [
+                    LdSelectItem(child: Text("Hello world"), value: "test"),
+                    LdSelectItem(child: Text("Hello world 2"), value: "test2")
+                  ],
+                ),
               ],
             ),
-            ldSpacerM,
-            LdToggle(
-                checked: _onSurface,
-                label: "On surface",
-                onChanged: (e) {
-                  setState(() {
-                    _onSurface = e;
-                  });
-                }),
-            ldSpacerM,
-            LdToggle(
-                checked: _disabled,
-                label: "Disabled",
-                onChanged: (e) {
-                  setState(() {
-                    _disabled = e;
-                  });
-                }),
-          ],
-        ),
-      ).padL(),
+          ),
+          LdToggle(
+              checked: _onSurface,
+              label: "On surface",
+              onChanged: (e) {
+                setState(() {
+                  _onSurface = e;
+                });
+              }),
+          LdToggle(
+              checked: _disabled,
+              label: "Disabled",
+              onChanged: (e) {
+                setState(() {
+                  _disabled = e;
+                });
+              }),
+        ],
+      ),
     );
   }
 }
