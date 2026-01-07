2f60e23838da9a563e38c3811bc4bbd41c69c857
Yannick <yannick.stolle@gmail.com>
2026-01-07 14:18:44 +0100

    chore: fix validation pipeline

--- packages/liquid_flutter/test/utils/compare_goldens.dart
+++ packages/liquid_flutter/test/utils/compare_goldens.dart
@@ -21,12 +21,6 @@ class LocalFileComparatorWithThreshold extends LocalFileComparator {
     );
 
     if (!result.passed && result.diffPercent <= threshold) {
-      debugPrint(
-        'A difference of ${result.diffPercent * 100}% was found, but it is '
-        'acceptable since it is not greater than the threshold of '
-        '${threshold * 100}%',
-      );
-
       return true;
     }
 
