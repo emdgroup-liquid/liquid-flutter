d07097654d35d3b2b90f7cb17b9de4be18354b7d
Felix <mail@flx.es>
2026-02-12 16:30:59 +0100

    fix(LdRepository): enhance optimistic filter tests with narrowing/broadening phases

--- packages/liquid_flutter/lib/src/monkey/data/repository.dart
+++ packages/liquid_flutter/lib/src/monkey/data/repository.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_flutter/liquid_flutter.dart';
-
 import 'package:provider/provider.dart';
 
 typedef FetchListWithParameters<T extends Identifiable<IdType>, IdType> = Future<LdListPage<T>> Function({
