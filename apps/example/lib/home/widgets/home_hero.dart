181fe5d13a669be6042b3c79b12794326634fcb8
Yannick Stolle <yannick.stolle@merckgroup.com>
2026-06-23 09:38:20 +0200

    chore:  center hero section properlz

--- apps/example/lib/home/widgets/home_hero.dart
+++ apps/example/lib/home/widgets/home_hero.dart
@@ -8,41 +8,44 @@ class HomeHero extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = LdTheme.of(context);
 
-    return LdAutoSpace(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          decoration: BoxDecoration(borderRadius: theme.radius(LdSize.m)),
-          clipBehavior: Clip.hardEdge,
-          child: Image.asset('liquid_flutter_icon.jpg', width: 64, height: 64),
-        ),
-        ldSpacerM,
-        Flexible(
-          child: LdAutoSpace(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              LdText.hl('Build for every platform \n with Liquid Flutter', textAlign: TextAlign.center),
-              LdMute(
-                child: LdText.ll(
-                  'Cross platform design system for Flutter. With first class support for desktop and mobile.',
-                ),
-              ),
-              Wrap(
-                spacing: 8,
-                runSpacing: 8,
-                children: [
-                  LdTag(child: Text('Web')),
-                  LdTag(child: Text('MacOS')),
-                  LdTag(child: Text('Windows')),
-                  LdTag(child: Text('Linux')),
-                  LdTag(child: Text('Android')),
-                  LdTag(child: Text('iOS')),
-                ],
-              ),
-            ],
+    return SizedBox(
+      width: double.infinity,
+      child: LdAutoSpace(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(borderRadius: theme.radius(LdSize.m)),
+            clipBehavior: Clip.hardEdge,
+            child: Image.asset('liquid_flutter_icon.jpg', width: 64, height: 64),
           ),
-        ),
-      ],
-    ).padL();
+          ldSpacerM,
+          Flexible(
+            child: LdAutoSpace(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                LdText.hl('Build for every platform \n with Liquid Flutter', textAlign: TextAlign.center),
+                LdMute(
+                  child: LdText.ll(
+                    'Cross platform design system for Flutter. With first class support for desktop and mobile.',
+                  ),
+                ),
+                Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  children: [
+                    LdTag(child: Text('Web')),
+                    LdTag(child: Text('MacOS')),
+                    LdTag(child: Text('Windows')),
+                    LdTag(child: Text('Linux')),
+                    LdTag(child: Text('Android')),
+                    LdTag(child: Text('iOS')),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ).padL(),
+    );
   }
 }
