b3512b9737c3f0574f4a4f79a8ca75ae93edeb5b
Yannick <yannick.stolle@gmail.com>
2026-08-06 08:48:13 +0200

    fix: spacing for headlines and dividers reduced

--- packages/liquid_flutter/lib/src/autospace.dart
+++ packages/liquid_flutter/lib/src/autospace.dart
@@ -52,8 +52,8 @@ List<Widget> generateAutoSpacings({
       (LdListItem _, LdListItem _) => _LdSizeItem(LdSize.s, 0),
       (LdText childText, LdText nextText) => switch ((childText.type, nextText.type)) {
           (LdTextType.headline, LdTextType.label) => _LdSizeItem(LdSize.xs, 1),
-          (LdTextType.headline, _) => _LdSizeItem(LdSize.l, 2),
-          (LdTextType.paragraph, LdTextType.headline) => _LdSizeItem(LdSize.l, 3),
+          (LdTextType.headline, _) => _LdSizeItem(LdSize.l, 1),
+          (LdTextType.paragraph, LdTextType.headline) => _LdSizeItem(LdSize.l, 2),
           (LdTextType.paragraph, LdTextType.paragraph) => _LdSizeItem(LdSize.m, 1),
           (_, LdTextType.label) => _LdSizeItem(LdSize.s, 1),
           (LdTextType.label, _) => _LdSizeItem(LdSize.s, 1),
@@ -64,7 +64,7 @@ List<Widget> generateAutoSpacings({
       (LdRadio _, LdRadio _) => _LdSizeItem(LdSize.xs, 1),
       (LdCheckbox _, LdCheckbox _) => _LdSizeItem(LdSize.xs, 1),
       (LdToggle _, LdToggle _) => _LdSizeItem(LdSize.xs, 1),
-      (LdDivider _, _) => _LdSizeItem(LdSize.l, 2),
+      (LdDivider _, _) => _LdSizeItem(LdSize.l, 1),
       (LdCard _, LdCard _) => _LdSizeItem(LdSize.l, 2),
       (LdDrawerItemSection _, LdDrawerItemSection _) => _LdSizeItem(LdSize.xs, 1),
       (LdSectionHeader _, LdSectionHeader _) => _LdSizeItem(LdSize.l, 1),
@@ -74,7 +74,7 @@ List<Widget> generateAutoSpacings({
           LdTextType.headline => _LdSizeItem(LdSize.l, 2),
           _ => _LdSizeItem(defaultSpacing, 1)
         },
-      (_, LdDivider _) => _LdSizeItem(LdSize.l, 1),
+      (_, LdDivider _) => _LdSizeItem(LdSize.m, 1),
       (_, _) => _LdSizeItem(defaultSpacing, 1),
     };
 
