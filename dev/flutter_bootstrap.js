3a12afeda04a7a70ea9910e7ad723e8dceac2054
ElectricCookie <ElectricCookie@users.noreply.github.com>
2026-08-07 09:37:50 +0000

    deploy: 1a75b9b348839e7c92da59310448085150126ccb

--- dev/flutter_bootstrap.js
+++ dev/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3082634390" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4033722919" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
